f15401acc2b369bd30900679bcfcefc9a0509571
taku0629 <takkuru2929@gmail.com>
2023-09-26 18:36:31 +0900

    Update memo.js

--- app/javascript/controllers/memo.js
+++ app/javascript/controllers/memo.js
@@ -1,12 +1,26 @@
 function post (){
   const form = document.getElementById("form");
-  form.addEventListener("submit",(e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST","/posts", true);
+    XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      const list = document.getElementById("list");
+      const item = XHR.response.post;
+      const html = `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+            ${item.content}
+          </div>
+        </div>`;
+      list.insertAdjacentHTML("afterend", html);
+    };
   });
 };
 
